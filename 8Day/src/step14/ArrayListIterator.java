83a706e71bb7af81f5d10996c892d8ffa8334761
liebeDY <hdydeveloper0927@gmail.com>
2020-11-24 21:04:54 +0900

    수업#1 [ArrayList_Iterator 예제]

---
+++ 8Day/src/step14/ArrayListIterator.java
@@ -0,0 +1,22 @@
+package step14;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListIterator {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> data = new ArrayList<String>();
+		
+		data.add("aaa");
+		data.add("bbb");
+		data.add("ccc");
+		
+		Iterator<String> i = data.iterator();
+		
+		while (i.hasNext()) {
+			System.out.println(i.next());
+		}
+	}
+}
