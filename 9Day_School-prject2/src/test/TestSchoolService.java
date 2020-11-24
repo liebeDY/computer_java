851df7edf72e91d0b2816f2e4f6df2acc8152ba0
liebeDY <hdydeveloper0927@gmail.com>
2020-11-24 22:17:28 +0900

    수업#6-7 [School-Project]

---
+++ 9Day_School-prject2/src/test/TestSchoolService.java
@@ -0,0 +1,40 @@
+package test;
+
+import service.SchoolService;
+import vo.Employee;
+import vo.Person;
+import vo.Student;
+import vo.Teacher;
+
+public class TestSchoolService {
+
+	public static void main(String[] args) {
+		
+		SchoolService service = new SchoolService();
+		
+		service.addPerson(new Teacher("123", "ㅁㅁㅁ", "서울", "국어"));
+		
+		Student s = new Student("234", "asdf", "안양", "S123");
+		service.addPerson(s);
+		
+		Employee e = new Employee("346", "oqweiu", "구리", "전산");
+		service.addPerson(e);
+		
+		service.printAll();
+		
+		Person per = service.findPerson("123");
+		if (per != null) {
+			System.out.println(per);
+		} else {
+			System.out.println("찾는 번호가 없습니다.");
+		}
+		System.out.println("--------------");
+		service.deletePerson("123");
+		service.printAll();
+		System.out.println("--------------");
+		service.updatePerson("234", new Student("010","ㅁㄴㅇㄹ", "종로", "13"));
+		service.printAll();
+	}	
+}
+
+
